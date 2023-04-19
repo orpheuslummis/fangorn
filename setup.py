f2342f5050e0298df441107f5cf2da15492fd3c9
Orpheus Lummis <o@orpheuslummis.info>
2023-04-19 21:24:14 +0000

    add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,26 @@
 from setuptools import find_packages, setup
 
-package_dir = {'': '.'}
+package_dir = {"": "."}
 
-package_data = {'': ['*']}
+package_data = {"": ["*"]}
+
+install_requires = ["jax", "numpyro", "optax", "seaborn", "matplotlib", "arviz", "geojson_pydantic"]
 
 setup_kwargs = {
-    'name': 'fangorn',
-    'version': '0.1.0',
-    'description': 'The core engine of Digital Gaia.',
-    'long_description': None,
-    'author': 'Digital Gaia, Inc',
-    'author_email': None,
-    'maintainer': None,
-    'maintainer_email': None,
-    'url': None,
-    'package_dir': package_dir,
-    'packages': find_packages(),
-    'package_data': package_data,
-    'python_requires': '>=3.8',
+    "name": "fangorn",
+    "version": "0.1.0",
+    "description": "The core engine of Digital Gaia.",
+    "long_description": None,
+    "author": "Digital Gaia, Inc",
+    "author_email": None,
+    "maintainer": None,
+    "maintainer_email": None,
+    "url": None,
+    "package_dir": package_dir,
+    "packages": find_packages(),
+    "package_data": package_data,
+    "python_requires": ">=3.10",
+    "install_requires": install_requires,
 }
 
 setup(**setup_kwargs)
